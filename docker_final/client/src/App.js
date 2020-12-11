dbded51b07801c33fec763db55f06353a80ce0ee
ajcm <mail@armandomarques.com>
2020-12-11 17:24:14 +0000

    123

---
+++ docker_final/client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+       <Fib/>
+        <div>
+          <Route exact path="/fib" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
